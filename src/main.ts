4aa900584f997aeebb83db9dd5d97d5c399e59b3
Stewart Orr <stewart.orr@gmail.com>
2026-03-04 16:42:39 +0000

    chore: remove old main.ts location

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-// src/main.ts
-import { soCalendar } from "./js/soCalendar";
-import "./scss/soCalendar.scss"
-document.addEventListener('DOMContentLoaded', (event) => {
-  const calendar = new soCalendar();
-});
